07599a97b5ebd6ba284fb2d28b8a485f33981f46
Aivan <biv888iii888@yandex.ru>
2023-05-09 21:38:08 +0300

    can_add_to_db

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from config import token, host, user, password, db_name
 print('я начал')
 bot = aiogram.Bot(token)
 dis = aiogram.Dispatcher(bot)
-
 try:
     connect = pymysql.connect(
         host=host,
@@ -20,9 +19,70 @@ try:
     )
 except:  print('ConnectionError')
 
+cur = connect.cursor()
+
+# def create_new_hero(tg_user_id):
+#     sql_code = f"INSERT INTO players tg_id VALUES {tg_user_id} "
+#     cur.execute(sql_code)
+#     #insert(table='players', all_coloniums=f'tg_id', all_values=f'{tg_user_id}')
+#     time = datetime.datetime.today().replace(microsecond=0)
+#     sql_code = f'SELECT user_id FROM players WHERE tg_id = {tg_user_id}'
+#     cur.execute(sql_code)
+#     local_user_id = cur.fetchall()[0]
+#     #local_user_id = select(what_return='user_id', where_column='tg_id', table='players', value=f'{tg_user_id}')[0]
+#     hero_id = 0
+#     hero_lvl = 0
+#     all_coloniums='`id`, `last_time`, `user_id`, `hero_id`, `hero_lvl`, `hero_activity`'
+#     all_values=f"NULL, '{time}', '{local_user_id}', '{hero_id}', '{hero_lvl}', '0'"
+#     table='`custom_heroes`'
+#     insert_query = f"INSERT INTO {table} ({all_coloniums}) VALUES ({all_values});"
+#     try:
+#         cur.execute(insert_query)
+#         next_hero_id = connect.insert_id() + 1
+#         print(next_hero_id)
+#
+#         connect.commit()
+#         print('стараюсь')
+#         aa = cur.lastword()
+#         print(aa)
+#         print('всё кул')
+#     except:
+#         print('говно')
+#     connect.commit()
+
 @dis.message_handler(commands=['start'])#прост отвечает
 async def start(message: aiogram.types):
-    await message.answer(text=start_text)
+    tg_user_id = message.from_user.id
+    #sql_code =f"CREATE TABLE `{db_name}` .`players`  ENGINE = InnoDB;"
+    sql_code = f"SELECT tg_id from players WHERE tg_id = {tg_user_id}"
+    cur.execute(sql_code)
+    result = list(cur.fetchall())
+    bb = [j for i in result for j in i]
+    #проверил есть ли пользователь. Если есть
+    if not bb:
+        await message.answer(text=new_reg_text)
+        try:
+            #create_player
+            sql_code = f"INSERT INTO `players` (`tg_id`, `money`) VALUES ('{tg_user_id}', '0')"
+            print(sql_code)
+            cur.execute(sql_code)
+            local_user_id = connect.insert_id()
+            connect.commit()
+            hero_id = 0
+            hero_lvl = 0
+            #create_hero
+            sql_code = f"INSERT INTO heroes (`id`, `last_time`, `user_id`, `hero_id`, `hero_lvl`, `hero_activity`) VALUES (NULL, NULL, '{local_user_id}', '{hero_id}', '{hero_lvl}', '0');"
+            print(sql_code)
+            cur.execute(sql_code)
+            connect.commit()
+            await message.answer(text=f'теперь ты зареган \n{commands}')
+        except:
+            await message.answer(text=f'админ пидорас сломал всё')
+
+    else:
+        await message.answer(text=f'{reg_text} {commands}')
+
+
 
 
 if __name__ == '__main__':
