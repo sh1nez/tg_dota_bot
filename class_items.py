434faeac9bfd360a5f3b86203dc3e9911fb08dcf
Aivan <biv888iii888@yandex.ru>
2023-05-16 10:47:51 +0300

    dochernii classi work

--- class_items.py
+++ class_items.py
@@ -5,37 +5,40 @@ class Item():
         self.index = index
     def send_item(self):
         return self.stat, self.price, self.index
-    class Farm_Items():
-        def __init__(self, farm, mobilit):
-            self.farm = farm
-            self.mobil_farm = mobilit
-        def send_farm(self):
-            return self.farm, self.mobil_farm
-    class Fight_items():
-        def __init__(self, helth, magic_damage, fiz_damage, esc, control):
-            self.hp = helth
-            self.mag_damage = magic_damage
-            self.fiz_damage = fiz_damage
-            self.esc = esc
-            self.control = control
-    class Ultimate_items():
-        def __init__(self, farm, mobility, esc, helth, magic_damage, fiz_damage, control):
-            self.hp = helth
-            self.mag_damage = magic_damage
-            self.fiz_damage = fiz_damage
-            self.esc = esc
-            self.farm = farm
-            self.mobil_farm = mobility
+class Farm_items(Item):
+    def __init__(self, price, main_stat, index, farm):#
+        super().__init__(price, main_stat,index,)
+        self.farm = farm
+        #self.mobil_farm = mobilit
+    def send_item(self):
+        return self.farm, self.price, self.stat, self.index
+class Fight_items(Item):
+    def __init__(self, price, main_stat, index, helth, damage,): #magic_ fiz_damage):#, esc, control):
+        super().__init__(price, main_stat, index)
+        self.hp = helth
+        self.damage = damage
+        #self.fiz_damage = fiz_damage
+        # self.esc = esc
+        # self.control = control
+class Ultimate_items(Item):
+    def __init__(self, price, main_stat, index, farm, helth, damage):#esc,, control, mobility,magic_damage, fiz_
+        super().__init__(price, main_stat, index)
+        self.hp = helth
+        self.damage = damage
+        #self.fiz_damage = fiz_damage
+        self.farm = farm
 
-topor =  Item(price=100, main_stat=0, index=0)\
-    .Farm_Items(farm=10, mobilit=1.1)
-midas = Item(price=2250, main_stat=0, index=1)\
-    .Farm_Items(farm=100, mobilit=1)
+topor = Farm_items(price=100, main_stat=0, index=0, farm=2)
+desolator = Fight_items(price=3500,main_stat=0, index=1, damage=20, helth=0)
+vetochka = Ultimate_items(price=50, main_stat=0, damage=1, index=2, helth=1, farm=1)
+
 test_class_items = [
     topor,
-    midas
+    desolator,
+    vetochka
 ]
-
-print(topor.send_farm())
+for i in test_class_items:
+    print(i.__dict__)
+#print(topor.send_farm())
 #print(Item.Farm_Items.topor)
 
