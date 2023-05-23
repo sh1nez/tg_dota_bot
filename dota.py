6ea05e795c021e98ea12044ba9634ee03dbcb38a
Aivan <biv888iii888@yandex.ru>
2023-05-24 00:06:45 +0300

    rdy fight (items*heroes)

--- dota.py
+++ dota.py
@@ -1,5 +1,6 @@
 class DifferentLens(Exception):
     """Нельзя использовать разные длины buffs"""
+
     def __init__(self, *args, **kwargs):
         print(*args, **kwargs)
         print('загляни в lvlup_hero, длины кортежей check')
@@ -7,7 +8,9 @@ class DifferentLens(Exception):
 
 class BaseHero:
     """def __init__(self, name: str, price: int, img1: str, img2: str, img3: str):"""
-    def __init__(self, hp: int, farm_speed: int, total_farm: int, fiz_tuple: tuple, magic_tuple: tuple):
+
+    def __init__(self, hp: int, farm_speed: int, total_farm: int, fiz_tuple: tuple, mag_tuple: tuple,
+                 magic_buf: float):
         # mag_hp: int, fiz_hp:int,
         self.hp = hp
 
@@ -15,9 +18,10 @@ class BaseHero:
         self.total_farm = total_farm
 
         self.fiz_tuple = fiz_tuple
-        self.mag_tuple = magic_tuple
+        self.mag_tuple = mag_tuple
+        self.mag_buf = magic_buf
 
-    def __lt__(self, *args, **kwargs) -> Exception:
+    def __mul__(self, other) -> Exception:
         return Exception('фигню сделал')
 
     @classmethod
@@ -27,18 +31,24 @@ class BaseHero:
     def lvlup_hero(self, *args, **kwargs) -> Exception:
         return Exception('не тот класс')
 
+    def no_items(self, *args, **kwargs) -> Exception:
+        return Exception('не тот класс')
+
 
 class NewHero(BaseHero):
     def __init__(self, name: str, price: int, description: str or None, img1, img2, img3,  # интерфейс
                  hp: int, fiz_armor: float, mag_armor: float,  # базовые хп
                  farm_speed: int, total_farm: int, kef_farm: float,
                  fiz_tuple: tuple, fiz_buf: float,  # attack_speed: float,
-                 magic_tuple: tuple, magic_buf: float,  # magic_tuple - (dmg, sec), (20, 0.57)
+                 mag_tuple: tuple, mag_buf: float,  # mag_tuple - (dmg, sec), (20, 0.57)
                  exp: int, lvl_up: dict):  # для lvl up
         """self, hp: int, farm_speed: int, total_farm: int,
-                 fiz_damage: int, attack_speed: float, magic_damage: int, magic_speed: float):"""
+                 fiz_damage: int, attack_speed: float, mag_damage: int, mag_speed: float):"""
         super().__init__(hp, farm_speed, total_farm,
-                         fiz_tuple, magic_tuple)  # base dmg
+                         fiz_tuple, mag_tuple, mag_buf)  # base dmg
+        """Stats"""
+        self.fiz_armor = fiz_armor  # 0-100
+        self.mag_armor = mag_armor  # 0-100
         """интерфейс"""
         self.name = name
         self.price = price
@@ -46,113 +56,140 @@ class NewHero(BaseHero):
         self.img = img1
         self.good = img2
         self.bad = img3
-        """Stats"""
-        self.fiz_armor = fiz_armor  # 0-100
-        self.mag_armor = mag_armor  # 0-100
         """коэффициенты"""
         self.kef_farm = kef_farm  # 0-n
         self.fiz_buf = fiz_buf  # 0-n
-        self.magic_buf = magic_buf  # 0-n
         """лвл апы"""
         self.exp = exp  # ~300-500
         self.lvl_up = lvl_up  # tuple buffs
 
-    def base_stats(self):
-        return
-
     def lvlup_hero(self, lvl) -> tuple:
         """Прибавки за уровень локального героя"""
-        """ exp, hp, fiz_armor, mag_armor, fiz_damage, attack_speed, (magic_damage, magic_speed)"""
-        if len(self.lvl_up['magic_tuple']) != len(self.mag_tuple):
+        """ exp, hp, fiz_armor, mag_armor, fiz_damage, attack_speed, (mag_damage, mag_speed)"""
+        if len(self.lvl_up['mag_tuple']) != len(self.mag_tuple):
             raise DifferentLens('магия длины кортежей')
         if len(self.lvl_up['fiz_tuple']) != len(self.fiz_tuple):
             raise DifferentLens('физика длины кортежей')
+
         mag = ()
-        for i in zip(self.mag_tuple, self.lvl_up['magic_tuple']):
-            mag += ((i[0][0]+(i[1][0]*lvl), i[0][1]-(i[1][1]*lvl)),)
+        for i in zip(self.mag_tuple, self.lvl_up['mag_tuple']):
+            mag += ((i[0][0] + (i[1][0] * lvl), i[0][1] - (i[1][1] * lvl)),)
         fiz = ()
         for i in zip(self.fiz_tuple, self.lvl_up['fiz_tuple']):
-            fiz += ((i[0][0]+(i[1][0]*lvl), i[0][1]+(i[1][1]*lvl)),)
-        """все источники маг урона изменены"""
-        print(self.fiz_armor, round((1 - self.fiz_armor), 5), self.lvl_up['fiz_armor'], lvl)
+            fiz += ((i[0][0] + (i[1][0] * lvl), i[0][1] + (i[1][1] * lvl)),)
+        """все источники урона изменены в зависимости от lvl"""
         fiz_armor = float(self.fiz_armor)
-        magic_armor = float(self.mag_armor)
+        mag_armor = float(self.mag_armor)
         for i in range(lvl):
-            fiz_armor += (1 - fiz_armor) * self.lvl_up['fiz_armor']
-            magic_armor += (1-fiz_armor) * self.lvl_up['mag_armor']
-        tup_hp = int(self.hp + self.lvl_up['hp'] * lvl), fiz_armor, magic_armor
-        tup_farm = self.farm_speed + self.lvl_up['farm_speed']*lvl,\
-            self.total_farm + self.lvl_up['total_farm']*lvl,
-        return tup_hp, tup_farm, fiz, mag
-
-    def exp_up_lvl(self, lvl, exp):
-        """Можем у героя вызвать этот класс, чтобы проверить эксп"""
-        exp_lvl = self.exp + self.lvl_up['exp'] * lvl
-        print(lvl, exp_lvl, exp)
-        while exp >= exp_lvl:
-            lvl += 1
-            exp -= exp_lvl
-            exp_lvl += + self.lvl_up['exp']
-        return lvl, exp
+            fiz_armor = round(fiz_armor + (100 - fiz_armor)/100 * self.lvl_up['fiz_armor'], 5)
+            mag_armor = round(mag_armor + (100 - fiz_armor)/100 * self.lvl_up['mag_armor'], 5)
+        tup_hp = int(self.hp + self.lvl_up['hp'] * lvl), fiz_armor, mag_armor,
+        tup_farm = self.farm_speed + self.lvl_up['farm_speed'] * lvl, self.total_farm + self.lvl_up['total_farm'] * lvl
+        return tup_hp, tup_farm, fiz, mag, self.mag_buf
 
 
 class LocalHero(BaseHero):
-    def __init__(self, hp: int, fiz_armor: float, magic_armor: float, farm_speed: int, total_farm: int,
-                 fiz_tuple: tuple, magic_tuple: tuple,):
-        """ hp: int, farm_speed: int, total_farm: int, fiz_tuple: tuple, magic_tuple: tuple):"""
-        super().__init__(hp, farm_speed, total_farm, fiz_tuple, magic_tuple)
-        self.magic_armor = magic_armor
+    def __init__(self, hp: int, fiz_armor: float, mag_armor: float, farm_speed: int, total_farm: int,
+                 fiz_tuple: tuple, mag_tuple: tuple, magic_buf: float):
+        """ hp: int, farm_speed: int, total_farm: int, fiz_tuple: tuple, mag_tuple: tuple):"""
+        super().__init__(hp, farm_speed, total_farm, fiz_tuple, mag_tuple, magic_buf)
+        self.mag_armor = mag_armor
         self.fiz_armor = fiz_armor
 
-    def __lt__(self, other: object) -> bool or Exception:
-        if not isinstance(other, LocalHero):
-            return Exception('нужен LocalHero')
-        return False
+    def no_items(self):
+        dick = {
+            'hp': self.hp,
+            'farm_speed': self.farm_speed,
+            'total_farm': self.total_farm,
+            'fiz_tuple': self.fiz_tuple,
+            'mag_tuple': self.mag_tuple,
+            'mag_buf': self.mag_buf,
+            'mag_armor': self.mag_armor,
+            'fiz_armor': self.fiz_armor,
+
+        }
+        return dick
+
+    @staticmethod
+    def battle(dick1, dick2):
+        """быть"""
+        # я хочу использовать die second, но мне нужны данные в формате проходящий урон
+        print()
+        # print(dick1['fiz_tuple'])
+        # print(dick2['fiz_tuple'][0][0])
+        # print(dick2['fiz_armor'])
+        # print(dick2['fiz_tuple'][0][0] * (100-dick2['fiz_armor'])/100)
+        fiz_dmg1 = [[dick2['fiz_tuple'][0][0] * (100-dick2['fiz_armor'])/100, i[1], 0] for i in dick1['fiz_tuple']]
+        mag_dmg1 = [[round(i[0] * (100 - dick2['mag_armor']/100), 5), i[1], 0] for i in dick1['mag_tuple']]
+        fiz_dmg2 = [[dick1['fiz_tuple'][0][0] * (100-dick1['fiz_armor'])/100, i[1], 0] for i in dick2['fiz_tuple']]
+        mag_dmg2 = [[round(i[0] * (100 - dick1['mag_armor']/100), 5), i[1], 0] for i in dick2['mag_tuple']]
+        print(dick2['hp'], fiz_dmg1+mag_dmg1)
+        print(dick1['hp'], fiz_dmg2+mag_dmg2)
+        seconds1 = die_second1(dick2['hp'], fiz_dmg1)  # + mag_dmg1)
+        seconds2 = die_second1(dick1['hp'], fiz_dmg2)  # + mag_dmg2)
+        return seconds1, seconds2
 
 
 class BaseItem:
-    def __init__(self, hp: int, fiz_armor: float, mag_armor: float,
-                 fiz_dmg: int, attack_speed: float, mag_dmg: int, mag_speed: float,
-                 farm_speed: int, total_farm: int,):
+    def __init__(self, hp: int or None, fiz_armor: float or None, mag_armor: float or None,
+                 fiz_tuple: tuple or None, mag_tuple: tuple or None, mag_buf: float or None,
+                 farm_speed: int or None, total_farm: int or None, ):
         self.hp = hp
-        self.fiz_armor = fiz_armor
-        self.mag_armor = mag_armor
-        self.fiz_dmg = fiz_dmg
-        self.attack_speed = attack_speed
-        self.mag_dmg = mag_dmg
-        self.mag_speed = mag_speed
         self.farm_speed = farm_speed
         self.total_farm = total_farm
+        self.fiz_tuple = fiz_tuple
+        self.mag_tuple = mag_tuple
+        self.mag_buf = mag_buf
+        self.fiz_armor = fiz_armor
+        self.mag_armor = mag_armor
 
-    def __add__(self, other: LocalHero):
-        if not isinstance(other, LocalHero):
-            raise Exception('можно прибавлять только предметы к герою')
-        print(LocalHero)
+    def __mul__(self, *args, **kwargs) -> Exception:
+        return Exception('не тот класс')
 
-    def __radd__(self, other):
-        self.__add__(other)
+
 class ShopItem(BaseItem):
-    def __init__(self, price: int, description: str or None, img1: str, img2: str,
-                 main_stat: int,
-                 hp: int, fiz_armor: float, mag_armor: float,
-                 fiz_dmg: int, attack_speed: float, mag_dmg: int, mag_speed: float,
-                 farm_speed: int, total_farm: int,
-                 ):
-        super().__init__(hp, fiz_armor, mag_armor, fiz_dmg, attack_speed, mag_dmg,
-                         mag_speed, farm_speed, total_farm)
+    def __init__(self, price: int, description: str or None, img1: str or None, img2: str or None,
+                 main_stat: int or None,
+                 hp: int or None, fiz_armor: float or None, mag_armor: float or None,
+                 fiz_tuple: tuple or None, mag_tuple: tuple or None, mag_buf: float or None,
+                 farm_speed: int or None, total_farm: int or None,):
+        super().__init__(hp, fiz_armor, mag_armor, fiz_tuple, mag_tuple, mag_buf, farm_speed, total_farm)
         self.main_stat = main_stat
         self.price = price
         self.description = description
         self.img1 = img1
         self.img2 = img2
 
+    def __mul__(self, other: dict) -> dict:
+        end_fiz_tup = ()
+        """Вернёт изменённую в зависимости от героя информацию для создания FightItem"""
+        for i in other['fiz_tuple']:
+            # print(i[0])
+            fiz_damage = i[0] + self.fiz_tuple[0] if self.fiz_tuple[0] else i[0]
+            fiz_speed = i[1] - self.fiz_tuple[1] if self.fiz_tuple[1] else i[1]
+            end_fiz_tup += ((fiz_damage, fiz_speed),)
 
-class FightItems(BaseItem):
-    def __init__(self, hp: int, fiz_armor: float, mag_armor: float,
-                 fiz_dmg: int, attack_speed: float, mag_dmg: int, mag_speed: float,
-                 farm_speed: int, total_farm: int,):
-        super().__init__(hp, fiz_armor, mag_armor, fiz_dmg, attack_speed, mag_dmg,
-                         mag_speed, farm_speed, total_farm)
+        end_mag_tup = ()
+        """тут нельзя нечего умножать иначе когда будет 6 предметов будет *6 kef главного атрибута"""
+        for i in other['mag_tuple']:
+            mag_damage = (self.mag_tuple[0] + other['mag_tuple'][0]) \
+                if self.mag_tuple[0] else i[0]
+            mag_speed = self.mag_tuple[0] - other['mag_tuple'][0] if self.mag_tuple[1] else i[1]
+            end_mag_tup += ((mag_damage, mag_speed),)
+
+        dick = {
+            'hp': other['hp'] + self.hp if self.hp else other['hp'],
+            'farm_speed': other['farm_speed'] + self.farm_speed if self.farm_speed else other['farm_speed'],
+            'total_farm': other['total_farm'] + self.total_farm if self.total_farm else other['total_farm'],
+            'fiz_tuple': end_fiz_tup,
+            'mag_tuple': end_mag_tup,
+            'mag_buf': self.mag_buf + other['mag_buf'] if self.mag_buf else other['mag_buf'],
+            'mag_armor': round(other['mag_armor'] + (self.mag_armor * (100 - other['fiz_armor']) / 100), 5)
+            if self.mag_armor else other['mag_armor'],
+            'fiz_armor': round(self.fiz_armor + (other['fiz_armor'] * (100 - other['fiz_armor']) / 100), 5)
+            if self.fiz_armor else other['fiz_armor'],
+        }
+        return dick
 
 
 def die_second1(hp: int, timers: list) -> int and float:
@@ -161,23 +198,22 @@ def die_second1(hp: int, timers: list) -> int and float:
     seconds = 0
     timers.sort(key=lambda k: k[1])
     small = timers[0][1]
+    a = len(str(small))
+    b = len(str(int(small)))
+    row = a-b-1
+
     while dmg < hp:
         for i in timers:
             if seconds // i[1] > i[2]:
                 i[2] += 1
-                dmg += i[0]
+                dmg = round(dmg+i[0], 5)
             else:
                 break
-        seconds = round(seconds+small, 5)
+        seconds = round(seconds + small, row)
     return dmg, seconds
 
 
 def die_second2(hp: int, timers: list) -> float:
     """test_arr = [[22, 0.47123, 0], [10, 0.3331212, 0], [100, 12.123123,  0], [321, 10.81283, 0]]"""
-    speeds = [i[0]/i[1] for i in timers]
-    return hp/sum(speeds)
-
-
-# test_arr = [[22, 0.47123, 0], [10, 0.3331212, 0], [100, 12.123123,  0], [321, 10.81283, 0]]
-# print(die_second1(999999, test_arr))
-# print(die_second2(999999, test_arr))
+    speeds = [i[0] / i[1] for i in timers]
+    return round(hp / sum(speeds), 3)
--- texts.py
+++ texts.py
@@ -1,32 +1,52 @@
-from dota import NewHero, LocalHero
-sf_stats = (
-    *('url', 'url2', 'url3'),
-    *(500, 0.22, 0.22,),  # hp, fiz_armor, mag_armor, от
-    *(250, 150, 1.1),  # фарм, в мс, тотал и кефе
-    *(((150, 10),), 0.7, ((100, 30,), (300, 20)), 1.488),
-    # fiz_damage: int, attack_speed: float, fiz_buf: float, magic_tuple, mag_buff
-    1000,  # базовый exp
-    {'exp': 300, 'hp': 50, 'fiz_armor': 0.8, 'mag_armor': 0.7,
-     'fiz_tuple': ((10, 2),), 'magic_tuple': ((7, 0.21), (21, 0.27)),
-     'total_farm': 5, 'farm_speed': 1, }
-    # можно уменьшат кд, можно не уменьшать добавив 0
-)
+from dota import NewHero, LocalHero, ShopItem  # FightItems
 
-sf = NewHero('негр', 80000, None, *sf_stats)
-hp, farm, fiz_dmg, mag_dmg = sf.lvlup_hero(5)
-print(sf.__dict__)
-#print(hp, farm, fiz_dmg, mag_dmg, sep='\n')
-local_hero = LocalHero(*hp, *farm, fiz_dmg, mag_dmg)
-print(local_hero.__dict__)
-"""изменены атрибуты в формате хп, физ armor, magic armor, кортеж физ урон, маг урон"""
+dick = {'exp': 300, 'hp': 50, 'fiz_armor': 1, 'mag_armor': 1,
+        'fiz_tuple': ((10, 2),), 'mag_tuple': ((7, 0.21), (21, 0.27),),  # можно уменьшат кд (0 - не уменьшать)
+        'total_farm': 5, 'farm_speed': 1, }
+sf = NewHero(name='негр', price=80000, description=None, img1=None, img2=None, img3=None,
+             hp=500, fiz_armor=15, mag_armor=15,
+             farm_speed=250, total_farm=150, kef_farm=1.2, fiz_tuple=((50, 1),), mag_tuple=((100, 30,), (300, 20),),
+             fiz_buf=0.7, mag_buf=1.4, exp=1000,  lvl_up=dick)
+'''
+(def __init__(self, price: int, description: str or None, img1: str, img2: str or None,
+                 main_stat: int or None,
+                 hp: int or None, fiz_armor: float or None, mag_armor: float or None,
+                 fiz_tuple: tuple or None, mag_tuple: tuple or None, mag_buf: float or None,
+                 farm_speed: int or None, total_farm: int or None,
+                 ):
 
-'''хп, физ armor, маг armor, физ урон физ ас маг урон маг ас '''
+midas_stats = (2250, 'IMBA', 'img', None,    # price desc img1 img2
+               None, 100, 5, 5,  # stat, hp, fiz_armor, mag_armor
+               (None, None,), (None, None,),  # (fiz_tup,), (mag_tup,)
+               None, None, None)  # mag_buf, farm_speed, total_far
+'''
+midas = ShopItem(price=2250, description=None, img1=None, img2=None, main_stat=None, hp=500,
+                 fiz_armor=None, mag_armor=None, fiz_tuple=(None, 0.2), mag_tuple=(None, None),
+                 mag_buf=None, farm_speed=None, total_farm=None)
+"""price=0, description=None, img1=None, img2=None, main_stat=None, hp=None,
+               fiz_armor=None, mag_armor=None, fiz_tuple=(None, None), mag_tuple=(None, None),
+               mag_buf=None, farm_speed=None, total_farm=None"""
+mom = ShopItem(price=0, description=None, img1=None, img2=None, main_stat=None, hp=None,
+               fiz_armor=None, mag_armor=None, fiz_tuple=(None, None), mag_tuple=(None, None),
+               mag_buf=None, farm_speed=None, total_farm=None)
 
-"""name, price, description, img, good_event, bad_event, magic_damage_kef, fiz_damage_kef,
-    total_mag_d, total_fiz_d, farm, total_farm, hp, lvl_up"""
+hp, farm, fiz_dmg, mag_dmg, mag_buf = sf.lvlup_hero(5)
+# print(hp, farm, fiz_dmg, mag_dmg, mag_buf, sep='\n')
+local_hero1 = LocalHero(*hp, *farm, fiz_dmg, mag_dmg, mag_buf)
+local_hero2 = LocalHero(*hp, *farm, fiz_dmg, mag_dmg, mag_buf)
 
+hero_dick1 = midas * local_hero1.__dict__
+hero_dick2 = local_hero2.no_items()
+print(hero_dick1)
+print(hero_dick2)
+# сейчас у меня есть
+print(LocalHero.battle(hero_dick1, hero_dick2))
 
-
+# hero_dick2 = sf.no_items()
+# сейчас есть словарь со всей боевой информацией
+# нужен статик метод который примет 2 словаря и вернёт 2 кортежа
+# print(hero_dick)
+# print(local_hero.battle(hero_dick, hero_dick))
 '''for i in items:
     buf = i.stats()
     for j in buf:
@@ -38,7 +58,6 @@ end_farm = int(pisapopa['total_farm']*pisapopa['kef_farm'])
 end_mag = int(pisapopa['total_mag']*pisapopa['kef_mag'])
 end_fiz = int(pisapopa['total_fiz']*pisapopa['kef_fiz'])
 '''
-
 
 '''# действия
 pisapopa = pudge.local_dick()  # сначала для рассчёта создам локальный словарь который будем насиловать
@@ -53,10 +72,7 @@ print(pisapopa)
 # сейчас мы должны прибавить бафы итемов
 items = (midas, desolator)'''
 
-
-
-
-next_text = """ ты зареган 
+next_text = """ ты зарегистрирован 
 доступные команды:
 /profile
 /shop
@@ -64,7 +80,7 @@ next_text = """ ты зареган
 
 gold_user = 'голда выдана, итого сейчас'
 
-reg_text = 'ты уже зареган\n'
+reg_text = 'ты уже зарегистрирован\n'
 
 enemy_click = [
     'пидор по своим кнопкам кликай',
@@ -79,8 +95,7 @@ commands = '''/gold
 /shop
 '''
 
-
-
+"""
 hero_dick = {
     0: {'name': 'pudge', 'description': 'самый секс перс доты имба покупай', 'price': 8800,
         'stats': 'asasd', 'img':r'https://cq.ru/storage/uploads/images/1530144/1.jpg',
@@ -90,12 +105,10 @@ hero_dick = {
         'event_img': r'https://cojo.ru/wp-content/uploads/2022/12/pudzh-kompendium-2020-1.webp', },
 
     2: {'name': 'sf', 'description': 'негр', 'price':10000,
-        'stats':'НЕГР', 'img':r'https://cojo.ru/wp-content/uploads/2022/12/pudzh-kompendium-2020-1.webp',
+        'stats': 'НЕГР', 'img':r'https://cojo.ru/wp-content/uploads/2022/12/pudzh-kompendium-2020-1.webp',
         'event_img': r'https://cojo.ru/wp-content/uploads/2022/12/pudzh-kompendium-2020-1.webp'},
 }
-#шаблон
-    #{'name': '', 'global_id': 0, 'price': 0, 'description': '', 'dis_stats': '', 'farm': 1 },
-#всё это нужно для описания, другие классы в другом файле для работы
+
 item_dick = {
     'farm': {
         0: {'name': 'топорик', 'global_id': 0, 'price': 100, 'description': 'шшаа', 'dis_stats':'немного фарма', 'farm': 5},
@@ -120,6 +133,7 @@ all_items ={
     3: {'name': 'лотар', 'price': 2700, 'description': 'неуязвимость', 'dis_stats': 'нет', 'fignt':15, 'farm':0},
     4: {'name': 'веточка', 'price': 50, 'description': 'имба', 'dis_stats': 'много статов', 'farm': 1, 'fight': 1 },
     }
+
 # photo_links_for_shop = [
 #     r'https://cojo.ru/wp-content/uploads/2022/12/pudzh-kompendium-2020-1.webp',
 #     r'https://mmo13.ru/download/content/202004/15/11/image_5e96c4956372a8.29507748.jpg?1586944173',
@@ -144,7 +158,6 @@ images = {
     }
 }
 
-"""
 пудж https://hsto.org/getpro/habr/post_images/6fc/750/e38/6fc750e38c21f9dc6a777c15cbf4be43.jpg
 сф https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvL1HZYHXfLVYE6DrWT98DujBzDquKy8f0hLKKcvqudndYk4_GwOu_GbQoB7lgW91Naxw&usqp=CAU
 ам(?) https://mmo-obzor.ru/_bd/1/27135127.jpg
@@ -152,8 +165,5 @@ images = {
 вс https://avatars.dzeninfra.ru/get-zen_doc/3504072/pub_5ed0e87d7c1a732a1551b54a_5ed0fc29335fdd4d23a738bb/scale_1200
 шторм https://static.wikia.nocookie.net/dota2_gamepedia/images/e/e7/Cosmetic_icon_Corridan_Maestro.png/revision/latest?cb=20161029000314 
  
-файт арт красиво
-
+арт красиво
 """
-
-#class Description_items: хз нужно или нет
